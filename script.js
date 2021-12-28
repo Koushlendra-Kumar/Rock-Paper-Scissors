7f0db9bb05135ac9dae4d48b1b203783c9c10931
Koushlendra Kuamr <code.koushlendra@gmail.com>
2021-12-28 13:04:34 +0530

    Add UI to the game

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+
+//Not a very clean code but it works
+const option = document.querySelectorAll('.option');
+option.forEach((option) => {
+  option.addEventListener('mouseenter', ()=> {
+    option.classList.add('selected');
+    option.addEventListener('mouseleave', () =>{
+      option.classList.remove('selected');
+    });
+  });
+});
+
 function computerPlay(){
   let computerChoice = Math.floor(Math.random() * 3);
   if(computerChoice == 0){
@@ -5,51 +17,97 @@ function computerPlay(){
   }else if (computerChoice == 1) {
     return 'paper';
   }else {
-    return 'scissor';
+    return  'scissor';
+  }
+}
+function game(){
+  if(computerScore < 5 && playerScore < 5){
+    computerPlay();
+    playSingleRound();
   }
 }
 
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', ()=>{
+  playerChoice= 'rock';
+  game();
+  isGameOver(); 
+});
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', ()=>{
+  playerChoice= 'paper';
+  game();
+  isGameOver();
+});
+const scissor = document.querySelector('.scissor');
+scissor.addEventListener('click', ()=>{
+  playerChoice ='scissor'; 
+  game();
+  isGameOver();
+});
+
 let playerScore = 0;
 let computerScore = 0;
+const computer = document.querySelector('.computer');
+const yourScore = document.querySelector('.your-score');
+const compScore = document.querySelector('.computer-score'); 
 
 function playSingleRound(playerSelection, computerSelection) {
-  let playerChoice = prompt("Enter Your Selection Below:");
   playerSelection = playerChoice.toLowerCase();
   computerSelection = computerPlay().toLowerCase();
 
-
-  if(
+  if((computerScore <= 5 || playerScore <= 5)&&
     (playerSelection == 'rock' && computerSelection == 'scissor')||
     (playerSelection == 'paper' && computerSelection == 'rock')||
     (playerSelection == 'scissor' && computerSelection == 'paper')
+    
   ){
-          console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+          computer.style.cssText = 'color: green';
+          computer.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
           playerScore++;
-        }else if (playerSelection == computerSelection) {
-          console.log('Tie!');
-        }else if(
+          yourScore.textContent = `Your Score: ${playerScore}`;
+          compScore.textContent = `Computer Score: ${computerScore}`;
+        }else if ((playerSelection == computerSelection)&&
+                  (computerScore <= 5 || playerScore <= 5)        
+        ){
+          computer.style.cssText = 'color: rgb(7, 62, 243)';
+          computer.textContent = `Tie!`;
+          yourScore.textContent = `Your Score: ${playerScore}`;
+          compScore.textContent = `Computer Score: ${computerScore}`;
+        }else if((computerScore <= 5 || playerScore <= 5)&&
           (playerSelection == 'rock' && computerSelection == 'paper')||
           (playerSelection == 'paper' && computerSelection == 'scissor')||
           (playerSelection == 'scissor' && computerSelection == 'rock')
+          
         ){
-          console.log(`You loose! ${computerSelection} beats ${playerSelection}.`);
+          computer.style.cssText = 'color: red';
+          computer.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
           computerScore++;
-        }else{
-          alert('Invalid Input');
+          yourScore.textContent = `Your Score: ${playerScore}`;
+          compScore.textContent = `Computer Score: ${computerScore}`;
         }
 
 }
-
-let winner = (playerScore>computerScore)? 'You are smarter than computer.': 'Computer is smarter than you.';
-function game(){
-  console.log(playSingleRound());
-  console.log(playSingleRound());
-  console.log(playSingleRound());
-  console.log(playSingleRound());
-  console.log(playSingleRound());
-  console.log(`You scored ${playerScore} points.`);
-  alert(winner);
-  console.log(winner);
+function isGameOver(){
+  if(playerScore ===5|| computerScore===5){
+    return finalResult();
+  }
 }
+const result = document.querySelector('.result');
+const finalResult =function(){
+  if(playerScore>computerScore){
+    result.textContent = 'You Won!';
+    result.classList.add('winner');
+  }
+  if(playerScore==computerScore){
+    result.textContent =' Tie!';
+    result.classList.add('tie');
+  }
+  if(playerScore<computerScore){
+    result.textContent ='You Lost!';
+    result.classList.add('looser');
+  }
+};
 
-console.log(game());
+
+
